fd88cf578e8647749d141f715c587379f0e47b4e
Raul Armas <r4rmas@gmail.com>
2024-05-02 12:27:06 -0500

    trying something for safari iphone

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,8 +33,8 @@
 
   function updateTheme(selectedTheme: string | undefined) {
     if (selectedTheme) {
-      document.querySelector("html")!
-        .setAttribute("data-theme", selectedTheme)
+      const html = document.querySelector("html")
+      if (html) html.setAttribute("data-theme", selectedTheme)
     }
   }
 </script>
@@ -153,12 +153,12 @@
       <svg width="12px" height="12px" class="h-2 w-2 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg>
     </div>
     <ul class="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
-      <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Business" value="business"/></li>
-      <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Dim" value="dim"/></li>
-      <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Dracula" value="dracula"/></li>
-      <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Forest" value="forest"/></li>
-      <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Night" value="night"/></li>
-      <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Sunset" value="sunset"/></li>
+      <li><input bind:group={selectedTheme} type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Business" value="business"/></li>
+      <li><input bind:group={selectedTheme} type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Dim" value="dim"/></li>
+      <li><input bind:group={selectedTheme} type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Dracula" value="dracula"/></li>
+      <li><input bind:group={selectedTheme} type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Forest" value="forest"/></li>
+      <li><input bind:group={selectedTheme} type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Night" value="night"/></li>
+      <li><input bind:group={selectedTheme} type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Sunset" value="sunset"/></li>
     </ul>
   </div>
 </div>
