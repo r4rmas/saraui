506d7f6ebf67d1cf53fc3776d1e5d3af5d3d14a5
Raul Armas <r4rmas@gmail.com>
2024-05-02 14:15:02 -0500

    trying something for safari iphone

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -148,7 +148,7 @@
       Light theme
       <svg width="12px" height="12px" class="h-2 w-2 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg>
     </div>
-    <ul class="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
+    <ul class="z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
       <li><input type="radio" on:click={({ target }) => handleClick(target)} name="theme" class="btn btn-sm btn-block btn-ghost justify-start" aria-label="Autumn" value="autumn"/></li>
       <li><input type="radio" on:click={({ target }) => handleClick(target)} name="theme" class="btn btn-sm btn-block btn-ghost justify-start" aria-label="Cupcake" value="cupcake"/></li>
       <li><input type="radio" on:click={({ target }) => handleClick(target)} name="theme" class="btn btn-sm btn-block btn-ghost justify-start" aria-label="Emerald" value="emerald"/></li>
