fb8dcb29355d89373a2138820d69169ffc9389cb
Raul Armas <r4rmas@gmail.com>
2024-05-12 21:36:42 -0500

    update /utilities

---
+++ src/routes/icons/+page.svelte
@@ -0,0 +1,10 @@
+<div class="flex flex-col w-full">
+  <h2 class="title mt-4">Icons</h2>
+  <p class="mb-4">This section will show you how to customize icons.</p>
+  <p>As Sara is made with daisyUI (a Tailwind plugin) the best option to add icons is obviously another Tailwind plugin.</p>
+  <p>Using icons in a "plugin way", allow us to easily paint them with the theme colors.</p>
+  <p>To modify any icon of any Sara component you need to use icon classes provided by the <a href="https://github.com/egoist/tailwindcss-icons" class="link link-secondary">@egoist/tailwindcss-icons</a> package.</p>
+  <p class="mt-8">
+    type <span class="font-semibold">IconClassString</span> = any icon class from <a href="https://github.com/egoist/tailwindcss-icons" class="link link-secondary">@egoist/tailwindcss-icons</a> package
+  </p>
+</div>
