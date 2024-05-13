15526a0355c2bd7720b90ec97a168dd77cbb1f08
Raul Armas <r4rmas@gmail.com>
2024-05-13 16:41:29 -0500

    remove typos

--- src/routes/whats-next/+page.svelte
+++ src/routes/whats-next/+page.svelte
@@ -1,7 +1,7 @@
 <div class="flex flex-col w-full">
   <h2 class="title mt-4">What's next</h2>
   <p>
-    As Sara is an open source contribution, its progress depends of the community support.
+    As Sara is an open source contribution, its progress depends on the community support.
   </p> 
   <p class="mb-4">
     If you want to contribute, please consider leaving a <a href="https://github.com/r4rmas/saraui" class="link link-secondary">star in GitHub</a>.
